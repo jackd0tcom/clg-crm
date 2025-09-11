fe31ab0000401f57299a416002d721ba4bba8ab9
Jack Ball <ball.jack2376@gmail.com>
2025-09-11 10:07:01 -0600

    added auth0provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,22 @@ import "./index.css";
 import "./styles/App.css";
 import { Provider } from "react-redux";
 import store from "./store/store.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-qysee6dr6mhj3r6y.us.auth0.com"
+    clientId="zXikUQTJioiJxEuxgsnaDTIpSuO0hRHp"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </Auth0Provider>
 );
